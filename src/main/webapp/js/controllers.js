04dd94e3c40bf7f516565100a82f6b973bfaff6e
Eric Olander <eolander@the41st.com>
2015-11-18 21:25:28 -0700

    add spinner

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -1,15 +1,18 @@
 'use strict';
 
-var chordApp = angular.module('chordApp', ['chordApp.directives']);
+var chordApp = angular.module('chordApp', ['chordApp.directives', 'angularSpinner']);
 
 chordApp.filter('musicNotation', function(){
     return function(a){
         return a.replace(/b/g, '\u266D').replace(/#/g, '\u266F');
     };
-}).controller("ChordListCtrl", function ($scope, $http) {
+}).controller("ChordListCtrl", ['$scope', '$http', 'usSpinnerService', function ($scope, $http, usSpinnerService) {
   //    $scope.drawChordChart = drawChordChart;
   $scope.myData = {};
+  $scope.isDisabled = false;
   $scope.myData.doClick = function (item, event) {
+    usSpinnerService.spin('spinner-1');
+    $scope.isDisabled = true;
     $scope.myData.chords = [];
     var url, firstChar = $scope.myData.chord.trim().charAt(0);
     if (firstChar.match(/[abcdefgrs]/i)) {
@@ -26,13 +29,18 @@ chordApp.filter('musicNotation', function(){
 
     responsePromise.success(function (data, status, headers, config) {
       $scope.myData.chords = data;
+      usSpinnerService.stop('spinner-1');
+      $scope.isDisabled = false;
     });
     responsePromise.error(function (data, status, headers, config) {
       alert("AJAX failed!");
+      usSpinnerService.stop('spinner-1');
+      $scope.isDisabled = false;
     });
   };
   $scope.onChange = function () {
     $scope.myData.chord = $scope.myData.chord.replace(/b/g, '\u266D');
     $scope.myData.chord = $scope.myData.chord.replace(/#/g, '\u266F');
   }
-});
+
+}]);
