083d6bde0bb38acceb167afa476f122c53f3dbdb
Eric Olander <eolander@the41st.com>
2016-04-16 09:14:07 -0700

    update dependencies

--- project/build.scala
+++ project/build.scala
@@ -9,10 +9,10 @@ object ChordserverBuild extends Build {
   val Organization = "com.example"
   val Name = "chord-server"
   val Version = "0.1.0-SNAPSHOT"
-  val ScalaVersion = "2.11.7"
+  val ScalaVersion = "2.11.8"
   val ScalatraVersion = "2.4.0"
 
-  lazy val project = Project (
+  lazy val project = Project(
     "chord-server",
     file("."),
     settings = ScalatraPlugin.scalatraWithJRebel ++ scalateSettings ++ Seq(
@@ -21,7 +21,7 @@ object ChordserverBuild extends Build {
       version := Version,
       scalaVersion := ScalaVersion,
       resolvers += Classpaths.typesafeReleases,
-    //  ideaExcludeFolders := Seq(".idea",".idea_modules"),
+      //  ideaExcludeFolders := Seq(".idea",".idea_modules"),
       libraryDependencies ++= Seq(
         "ch.qos.logback" % "logback-classic" % "1.1.3" % "runtime",
         "org.scalatra" %% "scalatra" % ScalatraVersion,
@@ -32,19 +32,19 @@ object ChordserverBuild extends Build {
         "org.eclipse.jetty" % "jetty-plus" % "9.1.5.v20140505" % "container",
         "javax.servlet" % "javax.servlet-api" % "3.1.0",
         "org.scalatra" %% "scalatra-json" % ScalatraVersion,
-		"org.json4s"   %% "json4s-jackson" % "3.3.0",
+        "org.json4s" %% "json4s-jackson" % "3.3.0",
         "org.clapper" %% "grizzled-slf4j" % "1.0.2",
         "chords" %% "chords" % "1.0",
         "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
       ),
-      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
+      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile) { base =>
         Seq(
           TemplateConfig(
             base / "webapp" / "WEB-INF" / "templates",
-            Seq.empty,  /* default imports should be added here */
+            Seq.empty, /* default imports should be added here */
             Seq(
               Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
-            ),  /* add extra bindings here */
+            ), /* add extra bindings here */
             Some("templates")
           )
         )
