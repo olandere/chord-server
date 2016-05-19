7c9b628a949b46334b34eb97e396a6f96c2b9272
Eric Olander <eolander@the41st.com>
2016-05-19 11:28:24 -0700

    lots of js cleanup

--- src/main/webapp/filters/filters.js
+++ src/main/webapp/filters/filters.js
@@ -1,8 +1,9 @@
-'use strict';
+(function() {
+  'use strict';
 
-angular.module('chordApp.filters', []).
-  filter("transformSymbols", function () {
-  return function(text) {
-    return text.replace(/b/g, '\u266D').replace(/#/g, '\u266F');
-  }
-});
+  angular.module('chordApp.filters', []).filter('transformSymbols', function() {
+    return function(text) {
+      return text.replace(/b/g, '\u266D').replace(/#/g, '\u266F');
+    }
+  });
+})();
---
+++ src/main/webapp/services/tuningService.js
@@ -0,0 +1,34 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('chordApp')
+    .factory('tuningService', tuningService);
+
+  tuningService.$inject = ['$cookies'];
+
+  function tuningService($cookies) {
+
+    return {
+      getTunings: getTunings,
+      addTuning: addTuning
+    };
+
+    function getTunings() {
+      return $cookies.getObject('tunings');
+    }
+
+    function addTuning(tuning) {
+      var tunings = $cookies.getObject('tunings');
+      if (_.isArray(tunings)) {
+        tunings.push(tuning);
+        $cookies.putObject('tunings', _.uniq(tunings));
+      } else {
+        $cookies.putObject('tunings', [tuning]);
+      }
+    }
+
+  }
+
+})();
+
